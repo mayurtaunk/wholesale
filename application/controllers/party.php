7a99f4917a7021e7d6cc5233615fadfbb50fa0d7
Nivedita <nivi2.mitra@gmail.com>
2013-07-16 21:07:26 +0530

    errror in validation corrected

--- application/controllers/party.php
+++ application/controllers/party.php
@@ -44,7 +44,7 @@ class Party extends CI_Controller {
 		$this->form_validation->set_rules('name', 'Party Name', 'trim|required');
 		$this->form_validation->set_rules('address', 'Address', 'trim');
 		//$this->form_validation->set_rules('contact', 'Contact', 'trim');
-		$this->form_validation->set_rules('contact', 'Contact', 'required|regex_match[/^[0-9(),-]+$/]|xss_clean');
+		$this->form_validation->set_rules('contact', 'Contact', 'required|regex_match[/^[0-9(),]+$/]|xss_clean');
 
 		$query = $this->db->query("SELECT name, address, contact FROM parties WHERE id = $id");
 		$row = $query->result_array();
