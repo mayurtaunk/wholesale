5bfea5698ab1ea08eb506af6c91eb5de315138a0
Nimesh Ganatra <g.nimesh118@gmail.com>
2013-07-28 16:55:25 +0530

    Report is working room for improvements

--- application/controllers/reports/sale_report.php
+++ application/controllers/reports/sale_report.php
@@ -7,15 +7,37 @@ class Sale_report extends CI_controller {
 
 	function index() {
 		$canlog=$this->radhe->canlogin();
-		if ($canlog!=1)
-		{
+		if ($canlog!=1) {
 			redirect('main/login');
 		}
 
-		$data['product_id']   = 0;
-		$data['product_name'] = '';
-		$data['from_date']	  = date('d-m-Y');
-		$data['to_date']	  = date('d-m-Y');
+		if($this->input->post('submit')) {
+
+			$data['product_id']   = $this->input->post('product_id');
+			$data['product_name'] = $this->input->post('product_name');
+			$data['from_date'] 	  = $this->input->post('from_date');
+			$data['to_date']	  = $this->input->post('to_date');
+			
+			$sql = "SELECT DATE_FORMAT(S.datetime,'%d-%m-%Y') AS date, P.name AS product, SD.price, SD.quantity, ROUND(SD.price * SD.quantity, 2) as amount 
+			FROM sales S INNER JOIN sale_details SD ON S.id = SD.sale_id 
+			INNER JOIN purchase_details PD ON PD.id = SD.purchase_detail_id
+			INNER JOIN products P ON P.id = PD.product_id 
+			WHERE PD.product_id = ".$data['product_id']." AND
+				DATE_FORMAT(S.datetime, '%d-%m-%Y') >= '".$data['from_date']."' AND
+				DATE_FORMAT(S.datetime, '%d-%m-%Y') <= '".$data['to_date']."'";
+			//$this->firephp->info($sql); exit;
+
+			$query = $this->db->query($sql);
+			$data['rows'] = $query->result_array();
+
+		}
+
+		else {
+			$data['product_id']   = 0;
+			$data['product_name'] = '';
+			$data['from_date']	  = date('d-m-Y');
+			$data['to_date']	  = date('d-m-Y');
+		}
 
 		$data['page'] = "reports/sale_report";
 		$data['page_title'] = "Sale Report";
